f327921c750fb3ff913a65f2480377dc77c0ff08
Aubin Dugelet <aubin.dugelet.partner@decathlon.com>
2025-02-16 15:40:24 -0500

    Switching to WIP homepage

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -7,7 +7,7 @@ import Contact from '../components/contact/Contact.vue'
 
 <template>
     <main>
-        <Header />
+        <!--Header /-->
         <div class="img-container">
             <img id="cubes-jeu-accueil" src="/src/assets/img/accueil_cubes.jpg" />
             <div id="description-background">
@@ -16,9 +16,16 @@ import Contact from '../components/contact/Contact.vue'
                     <div id="ptite-barre" />
                     <h2>Orthopédagogue</h2>
                     <p id="montreal-description">Montréal</p>
+
+                    <h2 id="in-progress">Site en cours de construction...</h2>
+
+                    <div id="reseaux-sociaux">
+                      <a href="https://www.linkedin.com/in/julie-sol-636895132/" target="_blank"><i id="linkedin" class="fab fa-linkedin fa-xl icon"></i></a>
+                      <a href="https://www.instagram.com/0rthopedagogie/" target="_blank"><i id="instagram" class="fa-brands fa-instagram fa-xl icon"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
-        <Contact />
+        <!--Contact /-->
     </main>
 </template>
